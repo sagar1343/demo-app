4bad5a2fdb9d66a4264b7fdba1831b5fa4072dd0
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2024-08-25 21:17:16 +0530

    update the url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState("Failed");
 
   useEffect(() => {
-    fetch("/demo")
+    fetch("http://localhost:4000/demo")
       .then((res) => res.json())
       .then((jsonRes) => setMessage(jsonRes))
       .catch(() => console.log("error in request"));
